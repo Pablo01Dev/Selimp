d7360ed8647b46c65d9eaa1d7051611d9c8ef4f9
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-12-23 16:02:13 -0300

    ajustando

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './footer.module.css'; // Certifique-se de importar o arquivo CSS corretamente
-
+import Phone from '../../assets/phone.png';
+import Mail from '../../assets/mail.png';
+import Local from '../../assets/location.png';
 function Footer() {
   return (
     <section id="footer">
@@ -9,15 +11,15 @@ function Footer() {
         <footer className={styles.footerContainer}>
           <div className={styles.footerInfo}>
             <div className={styles.footerInfoItem}>
-              <img src="https://png.pngtree.com/png-vector/20201028/ourmid/pngtree-phone-icon-in-solid-circle-png-image_2380227.jpg" alt="phone" />
+              <img src={Phone} alt="phone" />
               <p>21 2605-5923 | 21 2614-0308</p>
             </div>
             <div className={styles.footerInfoItem}>
-              <img src="https://e7.pngegg.com/pngimages/947/916/png-clipart-e-mail-icon-email-computer-icons-symbol-mail-icon-miscellaneous-angle.png" alt="mail" />
+              <img src={Mail} alt="mail" />
               <p>contato@selimp.com.br</p>
             </div>
             <div className={styles.footerInfoItem}>
-              <img src="https://w7.pngwing.com/pngs/250/324/png-transparent-placeholder-pin-location-gps-point-position-essential-icon.png" alt="local" />
+              <img src={Local} alt="local" />
               <p>São Gonçalo - RJ</p>
             </div>
             <div className={styles.footerSocial}>
